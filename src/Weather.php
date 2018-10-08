281258f5896f3e246d55c78c779ed15abc971df4
john <508110504@qq.com>
2018-10-09 01:04:55 +0800

    给Weather.php中添加参数异常和http异常处理

--- src/Weather.php
+++ src/Weather.php
@@ -8,6 +8,8 @@
 namespace Qnyt\Weather;
 
 use GuzzleHttp\Client;
+use Overtrue\Weather\Exceptions\HttpException;
+use Overtrue\Weather\Exceptions\InvalidArgumentException;
 
 class Weather
 {
@@ -47,6 +49,14 @@ class Weather
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
+        if (!\in_array(\strtolower($format), ['xml', 'json'])) {
+            throw new \InvalidArgumentException('Invalid response format:' . $format);
+        }
+
+        if (!\in_array(\strtolower($type), ['base', 'all'])) {
+            throw new \InvalidArgumentException('Invalid type value(base/all): ' . $type);
+        }
+
         $query = array_filter([
             'key' => $this->key,
             'city' => $city,
@@ -54,11 +64,15 @@ class Weather
             'extensions' => $type
         ]);
 
-        $response = $this->getHttpClient()->get($url, [
-            'query' => $query,
-        ])->getBody()->getContents();
+        try {
+            $response = $this->getHttpClient()->get($url, [
+                'query' => $query,
+            ])->getBody()->getContents();
 
-        return 'json' === $format ? \json_decode($response, true) : $response;
+            return 'json' === $format ? \json_decode($response, true) : $response;
+        } catch (\Exception $e) {
+            throw new \HttpException($e->getMessage(), $e->getCode(), $e);
+        }
     }
 
 }
