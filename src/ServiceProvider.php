0c76b1471b2f440b892ca5b204278fb08f52df77
john <508110504@qq.com>
2018-10-10 00:55:11 +0800

    Laravel 扩展包的延迟注册

---
+++ src/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * User: john
+ * Date: 18-10-10
+ * Time: 上午12:52
+ */
+
+namespace Qnyt\Weather;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    protected $defer = true;
+
+    public function register()
+    {
+        $this->app->singleton(Weather::class, function () {
+            return new Weather(config('services.weather.key'));
+        });
+
+        $this->app->alias(Weather::class, 'weather');
+    }
+
+    public function provides()
+    {
+        return [Weather::class, 'weather'];
+    }
+}
