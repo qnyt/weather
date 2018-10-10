d566a4784dd0aa3806e9b9891c3fcc69a51bb812
qnyt <508110504@qq.com>
2018-10-10 03:31:39 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,8 +1,12 @@
 <?php
-/**
- * User: john
- * Date: 18-10-10
- * Time: 上午12:52
+
+/*
+ * This file is part of the qnyt/weather.
+ *
+ * (c) qnyt <508110504@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Qnyt\Weather;
--- src/Weather.php
+++ src/Weather.php
@@ -1,8 +1,12 @@
 <?php
-/**
- * User: john
- * Date: 18-10-9
- * Time: 上午12:28
+
+/*
+ * This file is part of the qnyt/weather.
+ *
+ * (c) qnyt <508110504@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Qnyt\Weather;
@@ -14,6 +18,7 @@ use Qnyt\Weather\Exceptions\InvalidArgumentException;
 class Weather
 {
     protected $key;
+
     protected $guzzleOptions = [];
 
     public function __construct($key)
@@ -22,7 +27,8 @@ class Weather
     }
 
     /**
-     * 获取guzzle的client
+     * 获取guzzle的client.
+     *
      * @return Client
      */
     public function getHttpClient()
@@ -31,7 +37,8 @@ class Weather
     }
 
     /**
-     * 设置guzzle的client的参数
+     * 设置guzzle的client的参数.
+     *
      * @param array $options
      */
     public function setGuzzleOptions($options)
@@ -50,28 +57,29 @@ class Weather
     }
 
     /**
-     * 获取天气
-     * @param string|int $city 城市名/高德地址位置 adcode，比如：“深圳” 或者（adcode：440300）；
-     * @param string $type 返回内容类型：base: 返回实况天气 / all:返回预报天气；
-     * @param string $format 输出的数据格式，默认为 json 格式，当 output 设置为 “xml” 时，输出的为 XML 格式的数据。
+     * 获取天气.
+     *
+     * @param string|int $city   城市名/高德地址位置 adcode，比如：“深圳” 或者（adcode：440300）；
+     * @param string     $type   返回内容类型：base: 返回实况天气 / all:返回预报天气；
+     * @param string     $format 输出的数据格式，默认为 json 格式，当 output 设置为 “xml” 时，输出的为 XML 格式的数据
      */
     public function getWeather($city, $type = 'base', $format = 'json')
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
         if (!\in_array(\strtolower($format), ['xml', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: ' . $format);
+            throw new InvalidArgumentException('Invalid response format: '.$format);
         }
 
         if (!\in_array(\strtolower($type), ['base', 'all'])) {
-            throw new InvalidArgumentException('Invalid type value(base/all): ' . $type);
+            throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
         }
 
         $query = array_filter([
             'key' => $this->key,
             'city' => $city,
             'output' => $format,
-            'extensions' => $type
+            'extensions' => $type,
         ]);
 
         try {
@@ -84,5 +92,4 @@ class Weather
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
     }
-
 }
